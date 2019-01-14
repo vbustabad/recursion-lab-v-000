f3ada721b780938217773bd7608457eeee65a891
Veronica Bustabad <vbustabad@gmail.com>
2019-01-14 04:21:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,9 +53,9 @@ function maxOf(array) {
 }
 
 function includesNumber(array, number) {
-  if (array.length === 1) && (array[0] === number) {
+  if (array.length === 1 && array[0] === number) {
     return true;
-  } else if (array.length > 1) && (array[0] === number) {
+  } else if (array.length > 1 && array[0] === number) {
     newArray = array.slice(1);
     includesNumber(newArray, number);
   } else {
