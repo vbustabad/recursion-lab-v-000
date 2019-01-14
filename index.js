62f4d4de096c177352e9aaac025fae5ed4255f61
Veronica Bustabad <vbustabad@gmail.com>
2019-01-14 01:26:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-// Code your solution here!
+function printString(myString) {
+  console.log(myString[0]);
 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+function reverseString(myString) {
+  console.log(myString[0]);
+
+  if (myString.length > 1) {
+    let mySubString = myString.substring(myString.length, 0);
+    reverseString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+function isPalindrome() {
+  console.log(myString[myString.length - 1]);
+
+  if (myString.length > 1) {
+    let mySubString = myString.substring(myString.length - 2, 0);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
