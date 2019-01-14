c3c5ca1cf9e32d0da10e1bc578b81406d47e20e4
Veronica Bustabad <vbustabad@gmail.com>
2019-01-14 02:38:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ function addUpTo(array, index) {
   if (array.length === 0) {
     return 0;
   } else {
-    return array[0] + addUpTo(array.slice(1));
+    return array[0] + addUpTo(array.slice(1), index);
   }
 }
